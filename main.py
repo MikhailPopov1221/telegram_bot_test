d400a75193c56916bd59b90e8e9dae99f019c345
Mikhail Popov <popov_muy@t.top-academy.ru>
2025-10-18 21:06:31 +0300

    тамагочи день 1

--- main.py
+++ main.py
@@ -12,10 +12,83 @@ BOT_TOKEN = os.getenv("TG_API_KEY")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-# Обработчик команды /start
+pets = {}
+
+main_kb = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [types.KeyboardButton(text="🌯 Покормить"), types.KeyboardButton(text="🥎 Поиграть")],
+        [types.KeyboardButton(text="💤 Спать"), types.KeyboardButton(text="📊 Статус")],
+        [types.KeyboardButton(text="⭕ Выход")]
+    ],
+    resize_keyboard=True
+    
+)
+
 @dp.message(Command("start"))
 async def start_handler(message: types.Message):
-    await message.answer("Привет! Ты запустил бота")
+    user_id = message.from_user.id
+    
+    if user_id not in pets:
+        new_pet = {
+            "name": "Pepsik 🐱‍🚀",
+            "hunger": 50,
+            "energy": 50,
+            "happiness": 50,
+        }
+        pets[user_id] = new_pet
+    
+    await message.answer(
+        f"Привет, {message.from_user.first_name}!\n"
+        f"Познакомься со своим питомцем: {pets[user_id]["name"]}!\n"
+        f"Позаботься о нём!",
+        reply_markup=main_kb
+    )
+
+@dp.message(F.text == "🥎 Поиграть")
+async def play_pet(message: types.Message):
+    user_id = message.from_user.id
+    if user_id not in pets:
+        await message.answer("Сначала запусти бота с помощью команды /start")
+        return
+    pet = pets[user_id]
+    pet["happiness"] = min(pet["happiness"] + 10, 100)
+    pet["energy"] = max(pet["energy"] - 15, 0)
+    await message.answer(f"{pet['name']} весело поиграл!")
+
+@dp.message(F.text == "🌯 Покормить")
+async def feed_pet(message: types.Message):
+    user_id = message.from_user.id
+    if user_id not in pets:
+        await message.answer("Сначала запусти бота с помощью команды /start")
+        return
+    pet = pets[user_id]
+    pet["hunger"] = min(pet["hunger"] + 10, 100)
+    pet["energy"] = max(pet["energy"] - 5, 0)
+    await message.answer(f"{pet['name']} вкусно покушал!")
+
+def progress_bar(value: int, length: int):
+    filled = int(value/100 * 10)
+    return "🟩" * filled + "⬛" * (length - filled)
+
+@dp.message(F.text == "📊 Статус")
+async def status_pet(message: types.Message):
+    user_id = message.from_user.id
+    if user_id not in pets:
+        await message.answer("Сначала запусти бота с помощью команды /start")
+        return
+    pet = pets[user_id]
+    
+    hun = pet['hunger']
+    en = pet['energy']
+    hap = pet['happiness']
+    
+    status = (
+        f"Статус вашего питомца {pet['name']}:\n"
+        f"Сытость: {hun}% {progress_bar(hun, 10)}\n"
+        f"Энергия: {en}% {progress_bar(en, 10)}\n"
+        f"Счастье: {hap}% {progress_bar(hap, 10)}\n"
+    )
+    await message.answer(status)
     
 async def main():
     print("Бот запущен...")
