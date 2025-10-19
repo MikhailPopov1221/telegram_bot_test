ea3984830266b69e500ad795a49824f0184f145b
Mikhail Popov <popov_muy@t.top-academy.ru>
2025-10-19 21:00:03 +0300

    TG - bot

---
+++ keyboards.py
@@ -0,0 +1,34 @@
+from aiogram import types
+
+
+BTN_FEED = "🌯 Покормить"
+BTN_PLAY = "🥎 Поиграть"
+BTN_SLEEP = "💤 Спать"
+BTN_STATUS = "📊 Статус"
+BTN_EXIT = "⭕ Выход"
+
+
+main_kb = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [types.KeyboardButton(text=BTN_FEED), types.KeyboardButton(text=BTN_PLAY)],
+        [types.KeyboardButton(text=BTN_SLEEP), types.KeyboardButton(text=BTN_STATUS)],
+        [types.KeyboardButton(text=BTN_EXIT)]
+    ],
+    resize_keyboard=True
+)
+
+remove_kb = types.ReplyKeyboardRemove()
+
+
+food_kb = types.InlineKeyboardMarkup(
+    inline_keyboard= [
+        [
+            types.InlineKeyboardButton(text="🥩 Стейк", callback_data="steak"),
+            types.InlineKeyboardButton(text="🍗 Индейка", callback_data="turkey")
+            ],
+        
+        [
+            types.InlineKeyboardButton(text="🥃 Дать попить", callback_data="water")
+            ]
+    ]
+)
