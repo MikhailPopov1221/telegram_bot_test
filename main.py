fee3ab63ec336f3210a2817dd70c815ba4045a95
Mikhail Popov <popov_muy@t.top-academy.ru>
2025-10-25 20:59:14 +0300

    bot db start developing

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import asyncio
 from config import BOT_TOKEN
 from handlers import register_handlers
 from scheduler import start_scheduler
+from db import init_db
 
 
 async def main():
+    print("Инициализируем базу данных")
+    await init_db()
     
     start_scheduler()
     print("Фоновые задачи запущены")
